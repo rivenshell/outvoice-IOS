d9915495465e1fb1981ffcc4247be829c717c62f
rivenshell <rivenshell111@gmail.com>
2025-02-23 15:45:35 -0500

    onboarding v0.1

---
+++ outvoice/Onboarding/onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  onboarding.swift
+//  outvoice
+//
+//  Created by Riv Sal on 2/23/25.
+//
+
+import SwiftUI
+
+struct onboarding: View {
+    var body: some View {
+        Text("Welcome to Outvoice")
+    }
+}
+
+#Preview {
+    onboarding()
+}
