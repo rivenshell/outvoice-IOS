34a8d399298e2ea00c3eeecba6635d2c14f62353
rivenshell <rivenshell111@gmail.com>
2025-02-24 07:04:56 -0500

    onboarding re-struct v0.1

---
+++ outvoice/Models/OnboardingPage.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingPage.swift
+//  outvoice
+//
+//  Created by Riv Sal on 2/24/25.
+//
+
+import Foundation
+
+struct OnboardingPage {
+    let title: String
+    let description: String
+    let image: String?
+}
