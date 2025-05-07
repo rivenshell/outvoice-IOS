064924690966d8280e3cf990df806ac25d1354ba
rivenshell <rivenshell111@gmail.com>
2025-05-07 08:06:29 -0400

    Bug Fix

---
+++ outvoice/Goth Text.swift
@@ -0,0 +1,27 @@
+//
+//  Goth Text.swift
+//  outvoice
+//
+//  Created by Riv Sal on 4/29/25.
+//
+
+import SwiftUI
+
+struct Goth_Text: View {
+    var body: some View {
+        
+        VStack {
+            Text("Hi")
+            Text("We have buisness to take care of.")
+        }
+        
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    Goth_Text()
+}
